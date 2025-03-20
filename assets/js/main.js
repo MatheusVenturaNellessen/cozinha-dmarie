c0751e2852c9842d400c5b66cd1d255635c359d4
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2025-03-19 21:14:35 -0300

    adicionar funcionalidades/interatividades ao site via JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,187 @@
-const yakisobaId = document.querySelector("#yakisoba-id").innerHTML;
-const yakisobaPrice = document.querySelector("#yakisoba-price");
+const sectionBag = document.querySelector('#section-bag');
 
-const karagueId = document.querySelector("#karague-id").innerHTML;
-const karaguePrice = document.querySelector("#karague-price");
+const yakisobaId = document.querySelector('#yakisoba-id').innerHTML;
+const yakisobaAddBag = document.querySelector('#yakisoba-add-bag');
 
-const executiveId = document.querySelector("#executive-id").innerHTML;
-const executivePrice = document.querySelector("#executive-price");
+const karagueId = document.querySelector('#karague-id').innerHTML;
+const karagueAddBag = document.querySelector('#karague-add-bag');
 
-const displayBag = document.querySelector(".display-bag");
+const executiveId = document.querySelector('#executive-id').innerHTML;
+const executiveAddBag = document.querySelector('#executive-add-bag');
 
-const counterBag = document.querySelector(".counter-bag");
+const displayBag = document.querySelector('.display-bag');
 
+const adderPrice = document.querySelector('.adder-price');
 
-yakisobaPrice.addEventListener('click', function(e) {
+yakisobaAddBag.addEventListener('click', function(e) {
+    const el = e.target;
+    const price = el.innerHTML;
+
+    displayYakisoba(yakisobaId, price);
+});
+
+karagueAddBag.addEventListener('click', function(e) {
     const el = e.target;
     const price = el.innerHTML;
     
-    display(yakisobaId, price);
-    bagPrice(price);
+    displayKarague(karagueId, price);
 });
 
-karaguePrice.addEventListener('click', function(e) {
+executiveAddBag.addEventListener('click', function(e) {
     const el = e.target;
     const price = el.innerHTML;
     
-    display(karagueId, price);
-    bagPrice(price);
+    displayExecutive(executiveId, price);
 });
 
-executivePrice.addEventListener('click', function(e) {
-    const el = e.target;
-    const price = el.innerHTML;
+function converterInNumber(value) {
+    return parseFloat(value.slice(3, value.length).replace(',', '.'));
+}
+
+let liForYakisoba = null;
+
+function createLiForYakisoba() {
+    if(!liForYakisoba) {
+        liForYakisoba = document.createElement('li');
+        liForYakisoba.classList.add('yakisoba-display');
+    }
+
+    return liForYakisoba;
+} 
+
+let liForKarague = null;
+
+function createLiForKarague() {
+    if(!liForKarague) {
+        liForKarague = document.createElement('li');
+        liForKarague.classList.add('karague-display');
+    }
+
+    return liForKarague;
+} 
+
+let liForExecutive = null;
+
+function createLiForExecutive() {
+    if(!liForExecutive) {
+        liForExecutive = document.createElement('li');
+        liForExecutive.classList.add('executive-display');
+    }
+
+    return liForExecutive;
+} 
+
+function createImg() {
+    const img = document.createElement('img');
+    img.src = '/assets/img/trash-icon.png';
+    img.alt = 'ícone de um lixo';
+
+    return img;
+}
+
+function createDeleteButton() {
+    const deleteButton = document.createElement('button');
+    img = createImg();
+    deleteButton.appendChild(img); 
+    return deleteButton;
+}
+
+var counterY = 0;
+
+function displayYakisoba(id, price) {
+    price = converterInNumber(price);
     
-    display(executiveId, price);
-    bagPrice(price);
-});
+    counterY += 1;
 
-function display(id, price) {
-    const li = document.createElement('li');
-    li.innerHTML = `${id}: <span>${price}</span>`;
-
+    const li = createLiForYakisoba();    
+    li.innerHTML = `${id}: ${counterY}und. R$ ${(price*counterY).toFixed(2)}`;
     displayBag.appendChild(li);
+    
+    const deleteButton = createDeleteButton();
+    li.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', e => {
+        deleteButton.parentElement.remove();
+
+        counterY = 0;
+        bagPrice();
+    });
+
+    bagPrice();
 }
 
-let counter = 0;
+var counterK = 0;
 
-function bagPrice(price) {
-    const converter = price.slice(3, price.length).replace(',', '.');
-    const priceInNumber = parseFloat(converter);
+function displayKarague(id, price) {
+    price = converterInNumber(price);
     
-    counter += priceInNumber;
+    counterK += 1;
+
+    const li = createLiForKarague();    
+    li.innerHTML = `${id}: ${counterK}und. R$ ${(price*counterK).toFixed(2)}`;
+    displayBag.appendChild(li);
     
-    counterBag.innerHTML = counter;
+    const deleteButton = createDeleteButton();
+    li.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', e => {
+        deleteButton.parentElement.remove();
+
+        counterK = 0;
+        bagPrice();
+    });
+
+    bagPrice();
 }
+
+var counterE = 0;
+
+function displayExecutive(id, price) {
+    price = converterInNumber(price);
+    
+    counterE += 1;
+
+    const li = createLiForExecutive();    
+    li.innerHTML = `${id}: ${counterE}und. R$ ${(price*counterE).toFixed(2)}`;
+    displayBag.appendChild(li);
+    
+    const deleteButton = createDeleteButton();
+    li.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', e => {
+        deleteButton.parentElement.remove();
+
+        counterE = 0;
+        bagPrice();
+    });
+
+    bagPrice();
+}
+
+function bagPrice() {
+    const total = (counterY * converterInNumber(yakisobaAddBag.innerHTML)) + (counterK * converterInNumber(karagueAddBag.innerHTML)) + (counterE * converterInNumber(executiveAddBag.innerHTML));
+
+    if(total > 0) {
+        adderPrice.innerHTML = total.toFixed(2);
+    } else {
+        adderPrice.innerHTML = '00,00';
+    }
+    
+}
+
+[yakisobaAddBag, karagueAddBag, executiveAddBag].forEach(el => {
+    el.addEventListener('click', e => {
+        if (!document.querySelector('.purchase-button')) {
+            const button = document.createElement('button');
+            button.classList.add('purchase-button')
+            button.innerHTML = 'Ir para o pagamento'
+        
+            sectionBag.appendChild(button);
+        
+            button.addEventListener('click', e => {
+                alert('Backend required!');
+            });
+        }
+    }, { once: true });  
+});
+
